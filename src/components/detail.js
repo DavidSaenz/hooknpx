1f51bbc1d8d576d57f833539cf3c3d89758ab006
David Saenz <jd.saenz1519@uniandes.edu.co>
2022-11-15 09:16:25 -0500

    commit 1

---
+++ src/components/detail.js
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+
+export default function Detail() {
+ const params = useParams();
+ return (
+   <div>
+     <h1>I am {params.mascotaId}</h1>
+     <h1>I am {params.mascotaId.Detail}</h1>
+     
+
+   </div>
+ );
+}
